bd817b9f8d43f0bbcd0f0ba5f8b42b9137d211ba
arafathpro3-spec <arafathpro3@gmail.com>
2026-09-25 10:17:48 +0100

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBN2Z5vBmHIQ6bv_vItRr4SoqqcuAa4cu0",
-  authDomain: "apexcrm-d42f4.firebaseapp.com",
-  projectId: "apexcrm-d42f4",
-  storageBucket: "apexcrm-d42f4.firebasestorage.app",
-  messagingSenderId: "173561404683",
-  appId: "1:173561404683:web:9d5cbe86e10fd03890d60e",
-  workspaceId: "data"
-};
